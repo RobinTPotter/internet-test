8a1845d0ea5af5dcc2022b82c77285a8777ac289
Your Name <you@example.com>
2021-03-11 11:25:44 +0000

    x

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # internet-test
 pings google, creates a big log and sends an email
 
-0 */3 * * * cd /home/pi/internet-test && ./pingres.sh
-* * * * * cd /home/pi/internet-test && flock -w 5 /tmp/pinglock /home/pi/pingrun.sh
+# results every 3 hours
+```0 */3 * * * cd /home/pi/internet-test && ./pingres.sh```
 
+# run the ping test unless locked (already running)
+```* * * * * cd /home/pi/internet-test && flock -w 5 /tmp/pinglock /home/pi/internet-test/pingrun.sh```
+
+# logrotate
+```0 0 * * 0 cd /home/pi/internet-test && sudo logrotate pinglogrot.conf```
+
+# Also server:
+
+install gunicorn for python3 (```sudo apt-get install gunicorn3```)
+
+```gunicorn3 --bind=0.0.0.0 pingflask:app```
--- pingtest.py
+++ pingtest.py
@@ -1,57 +1,66 @@
 from datetime import datetime
 
+def page():
+    
+    with open("pingtest.txt","r") as f:
+        data = f.readlines()
+    
+    data = [d.split() for d in data]
+    
+    data0 = [[int(d[0]),d[-2]] for d in data]
+    
+    data = []
+    drops = []
+    
+    
+    for d in data0:
+        if d[1]=='is': data.append([d[0],'no wifi'])
+        elif d[1]=='Net': data.append([d[0],'no internet'])
+        else: data.append([d[0],'ok'])
+    
+    # print(data[:10])
+    
+    status = None
+    start_drop = 0
+    
+    for d in data:
+        if status is None: status = d[1]
+        if d[1] != status:
+            if status == 'ok' and d[1] == 'no internet':
+                start_drop = d[0]
+            if status == 'no internet' and d[1] == 'ok':
+                drops.append([start_drop, d[0]-start_drop])
+    
+            status = d[1]
+    
 
-with open("pingtest.txt","r") as f:
-    data = f.readlines()
+    now = datetime.now()
+    
+    output = """<html>
+<head>
+<style>
+td.c {{ text-align: center }}
+th {{ text-align: center }}
+table, th, td {{ border: 1px solid black }}
+</style>
+</head>
+<body>
+{}
+<table>
+<thead>
+<th>time</th><th>minutes</th><th>seconds</th>
+</thead>
+""".format(now.strftime("%d/%m/%Y %H:%M:%S"))
+    
+    for d in drops:
+        output += '<tr><td>{}</td><td class="c">{}</td><td class="c">{}</td></tr>\n'.format(datetime.utcfromtimestamp(d[0]).strftime('%d/%m/%Y %H:%M:%S'), int(d[1]/60), d[1]-(60*int(d[1]/60)))
+    
+    output += """</table>
+</body>
+</html>
+"""
+    
+    return output
 
-data = [d.split() for d in data]
-
-data0 = [[int(d[0]),d[-2]] for d in data]
-
-data = []
-drops = []
-
-
-for d in data0:
-    if d[1]=='is': data.append([d[0],'no wifi'])
-    elif d[1]=='Net': data.append([d[0],'no internet'])
-    else: data.append([d[0],'ok'])
-
-# print(data[:10])
-
-status = None
-start_drop = 0
-
-for d in data:
-    if status is None: status = d[1]
-    if d[1] != status:
-        if status == 'ok' and d[1] == 'no internet':
-            start_drop = d[0]
-        if status == 'no internet' and d[1] == 'ok':
-            drops.append([start_drop, d[0]-start_drop])
-
-        status = d[1]
-
-print ('<html>')
-print ('<head>')
-print ('<style>')
-print ('td.c { text-align: center }')
-print ('th { text-align: center }')
-print ('table, th, td { border: 1px solid black }')
-
-print ('</style>')
-print ('</head>')
-print ('<body>')
-print ('<table>')
-
-print ('<thead>')
-print ('<th>time</th><th>minutes</th><th>seconds</th>')
-print ('</thead>')
-
-for d in drops:
-    print('<tr><td>{}</td><td class="c">{}</td><td class="c">{}</td></tr>'.format(datetime.utcfromtimestamp(d[0]).strftime('%d/%m/%Y %H:%M:%S'), int(d[1]/60), d[1]-(60*int(d[1]/60))))
-
-print ('</table>')
-print ('</body>')
-print ('</html>')
-
+if __name__ == "__main__":
+    print(page())
