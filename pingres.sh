3cc850d87f3978d6f659e09fb879c456bcde6883
Your Name <you@example.com>
2021-05-23 08:48:32 +0100

    wrong filename

--- pingres.sh
+++ pingres.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
-{ echo  "Subject: Outages\nFrom: pi3\nContent-Type: text/html; charset=\"utf-8\"\n" && python3 pingtest.py ; } | /usr/sbin/ssmtp robin.t.potter@googlemail.com
+{ echo  "Subject: Outages\nFrom: pi3\nContent-Type: text/html; charset=\"utf-8\"\n" && python3 pingtes2t.py ; } | /usr/sbin/ssmtp robin.t.potter@googlemail.com
 
